4681143d49365a127ba0ef1d6ed3aa47fed91fc5
Sneha Gautam <79215346+snehagautam869@users.noreply.github.com>
2023-10-04 19:33:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
-let name = "<b> Sneha Gautam </b>"
+/* Project 1 -- text utilities -- no of words,extrs space remove,capitilize,lower to upper etc i.e one text box where you upload your text  */
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className="contain">
-      <h1>Hello {name}</h1>      
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos amet iure pariatur ipsa incidunt ut quidem, porro rem saepe laudantium adipisci assumenda consectetur. Ratione quam corporis reprehenderit assumenda omnis consectetur aperiam quis quibusdam officia recusandae?
-      </p>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">TextUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
     </>
   );
 }
