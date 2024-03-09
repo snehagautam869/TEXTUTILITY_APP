cff4171d68230426cd79c2c2f8fc7b204e26705f
Sneha Gautam <79215346+snehagautam869@users.noreply.github.com>
2024-03-10 02:03:15 +0530

    Updates

---
+++ precache-manifest.b2288866d1e4ee4a3952fe01c0cd4737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314234f4dcaa5da13fd3f380fb6f9470",
+    "url": "/TEXTUTILITY_APP/index.html"
+  },
+  {
+    "revision": "dae0790ed605016306d3",
+    "url": "/TEXTUTILITY_APP/static/css/main.c0d410fb.chunk.css"
+  },
+  {
+    "revision": "e27c741e44b5249c1eaa",
+    "url": "/TEXTUTILITY_APP/static/js/2.e2ecdddf.chunk.js"
+  },
+  {
+    "revision": "50b01b64aedfea5f3a01b438bb59e228",
+    "url": "/TEXTUTILITY_APP/static/js/2.e2ecdddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df696ce08bdca4ef967d",
+    "url": "/TEXTUTILITY_APP/static/js/3.3cbdbf8f.chunk.js"
+  },
+  {
+    "revision": "dae0790ed605016306d3",
+    "url": "/TEXTUTILITY_APP/static/js/main.e44e5d1c.chunk.js"
+  },
+  {
+    "revision": "71377b0b131f7a723e2a",
+    "url": "/TEXTUTILITY_APP/static/js/runtime-main.4942b03d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TEXTUTILITY_APP/precache-manifest.b2288866d1e4ee4a3952fe01c0cd4737.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TEXTUTILITY_APP/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
